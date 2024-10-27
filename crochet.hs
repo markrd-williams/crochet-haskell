9a6833b80d701518191c9a13651407d98dc20124
Mark Williams <markrd.williams@gmail.com>
2024-10-27 11:15:35 +0000

    normalising commit

--- crochet.hs
+++ crochet.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE MultiWayIf #-}
+
 module Crochet where
 
+import Control.Monad (guard)
 import Data.Char (isAlpha, isNumber)
-import Data.List (delete, union, nub, inits, minimumBy)
+import Data.List (delete, inits, isPrefixOf, minimumBy, nub, union)
 import System.IO
 import Text.ParserCombinators.ReadP
 
@@ -18,7 +20,6 @@ data CrochetCode
 instance Semigroup CrochetCode where
   (<>) = Then
 
-
 instance Show CrochetCode where
   show :: CrochetCode -> String
   show NoOp = ""
@@ -28,50 +29,64 @@ instance Show CrochetCode where
   show (Then c1 c2) = show c1 ++ "\n" ++ show c2
   show (Seq n c) = "Seq " ++ show n ++ ":\n  " ++ concatMap (\s -> if s == '\n' then "\n  " else pure s) (show c)
 
-
 size :: CrochetCode -> Int
-size NoOp         = 0
-size SC           = 1
-size (Inc n)      = 1
-size (Dec n)      = 1
-size (Seq n c)    = 1 + size c
-size (Then c1 c2) = size c1 + size c2 + 10000
+size NoOp = 0
+size SC = 1
+size (Inc n) = 1
+size (Dec n) = 1
+size (Seq n c) = size c
+size (Then c1 c2) = size c1 + size c2
 
 unthen :: CrochetCode -> [CrochetCode]
 unthen (Then c1 c2) = unthen c1 ++ unthen c2
 unthen c = [c]
 
-thenReduce :: CrochetCode -> CrochetCode -> [CrochetCode]
-thenReduce NoOp c = [c]
-thenReduce c NoOp = [c]
-thenReduce c c' | c == c' = [Seq 2 c]
-thenReduce (Seq n c) (Seq m c') | c == c' = [Seq (n + m) c]
-thenReduce (Seq n c) (Seq m c') | n == m = [Seq n (c <> c')]
-thenReduce (Seq n c) c' | c == c' = [Seq (n + 1) c]
-thenReduce c' (Seq n c) | c == c' = [Seq (n + 1) c]
-thenReduce (Inc n) (Inc m) = [Inc (n + m)]
-thenReduce (Dec n) (Dec m) = [Dec (n + m)]
-thenReduce c c' = [c, c']
+thenReduce :: CrochetCode -> CrochetCode -> CrochetCode
+thenReduce NoOp c = c
+thenReduce c NoOp = c
+thenReduce c c' | c == c' = Seq 2 c
+thenReduce (Seq n c) (Seq m c') | c == c' = Seq (n + m) c
+thenReduce (Seq n c) (Seq m c') | n == m = Seq n (c <> c')
+thenReduce (Seq n c) c' | c == c' = Seq (n + 1) c
+thenReduce c' (Seq n c) | c == c' = Seq (n + 1) c
+thenReduce (Inc n) (Inc m) = Inc (n + m)
+thenReduce (Dec n) (Dec m) = Dec (n + m)
+thenReduce c c' = c <> c'
 
 minimumByKey :: (Ord b, Foldable t) => (a -> b) -> t a -> a
 minimumByKey f = minimumBy (\x y -> f x `compare` f y)
 
 splits :: [a] -> [([a], [a])]
 splits [] = [([], [])]
-splits (x:xs) = ([], x:xs) : [ (x : before, after) | (before, after) <- splits xs ]
+splits (x : xs) = ([], x : xs) : [(x : before, after) | (before, after) <- splits xs]
 
 thenJoin :: [CrochetCode] -> CrochetCode
 thenJoin [] = NoOp
-thenJoin xs = foldl1 (<>) xs
-
-
+thenJoin xs = foldl1 thenReduce (thenReduce'' xs)
 
 thenReduce' :: CrochetCode -> [CrochetCode] -> [CrochetCode]
 thenReduce' c [] = [c]
-thenReduce' c (x : xs) = thenReduce c x ++ xs
+thenReduce' c xs = minimumByKey (sum . map size) $ map (\(h, t) -> thenReduce c (thenJoin h) : t) (splits xs)
 
--- thenReduce'' :: [CrochetCode] -> CrochetCode
--- thenReduce'' xs = map (minimumByKey size . (\(h, t) -> thenReduce' (thenJoin h) t)) (splits xs)
+takeCopies :: (Eq a) => [a] -> [a] -> (Int, [a])
+takeCopies ps xs =
+  if ps `isPrefixOf` xs
+    then
+      (\(x, y) -> (x + 1, y)) (takeCopies ps $ drop (length ps) xs)
+    else
+      (0, xs)
+
+thenReduce'' :: [CrochetCode] -> [CrochetCode]
+thenReduce'' as = minimumByKey (sum . map size) (as : h as)
+  where
+    h :: [CrochetCode] -> [[CrochetCode]]
+    h as = do
+      (xs, zs) <- splits as
+      ys <- inits zs
+      guard $ not (null ys)
+      guard $ (ys ++ ys) `isPrefixOf` zs
+      let (n, zs') = takeCopies ys zs
+      pure $ xs ++ [Seq n (thenJoin ys)] ++ zs'
 
 norm :: CrochetCode -> CrochetCode
 norm NoOp = NoOp
@@ -81,7 +96,7 @@ norm (Inc n) = Inc n
 norm (Dec 0) = NoOp
 norm (Dec n) = Dec n
 norm (Then c1 c2) =
-  foldr1 (<>) (foldr thenReduce' [] un)
+  foldr1 (<>) (thenReduce'' un)
   where
     un = unthen (norm c1) ++ unthen (norm c2)
 norm (Seq 0 c) = NoOp
@@ -93,22 +108,21 @@ norm (Seq n c) =
     c' -> Seq n c'
 
 evenSpacing :: Int -> Int -> [Int]
-evenSpacing m n = [i * n `div` m + n `div` (2 * m) | i <- [0..m-1]]
-
+evenSpacing m n = [i * n `div` m + n `div` (2 * m) | i <- [0 .. m - 1]]
 
 crochetn :: Int -> CrochetCode
 crochetn x
   | x == 0 = Dec 1
   | x == 1 = SC
-  | x > 1  = SC <> Inc (x - 1)
-  | x < 0  = NoOp
+  | x > 1 = SC <> Inc (x - 1)
+  | x < 0 = NoOp
 
 crochetThese :: [Int] -> CrochetCode
 crochetThese = foldr ((<>) . crochetn) NoOp
 
 listToBag :: [Int] -> Int -> [Int]
 listToBag [] n = replicate n 0
-listToBag (x:xs) n = take x b ++ [(b !! x) + 1] ++ drop (x + 1) b
+listToBag (x : xs) n = take x b ++ [(b !! x) + 1] ++ drop (x + 1) b
   where
     b = listToBag xs n
 
@@ -119,8 +133,7 @@ nextRow' :: Int -> Int -> CrochetCode
 nextRow' current goal =
   if goal == 0
     then NoOp
-    else case
-     current `compare` goal of
+    else case current `compare` goal of
       EQ -> Seq current SC
       LT ->
         let per_hole = goal `div` current
@@ -131,11 +144,10 @@ nextRow' current goal =
               GT ->
                 let st_spacing = current `div` extra
                     extra_end = goal `mod` extra
-                 in
-                      Seq
-                        extra
-                        (Seq (st_spacing - 1) (SC <> Inc (per_hole - 1)) <> SC <> Inc per_hole)
-                        <> Seq extra_end (SC <> Inc extra_end)
+                 in Seq
+                      extra
+                      (Seq (st_spacing - 1) (SC <> Inc (per_hole - 1)) <> SC <> Inc per_hole)
+                      <> Seq extra_end (SC <> Inc extra_end)
       GT ->
         let per_stitch = current `div` goal - 1
             extra = current `mod` goal
@@ -159,27 +171,24 @@ neighbours vs ((i, j, k) : ts) =
     [0 ..]
     (neighbours vs ts)
 
-
 outerBoundary :: [Int] -> [Int] -> [[Int]] -> [Int]
 outerBoundary b s ns = nub [n | i <- b, n <- ns !! i, n `notElem` s]
 
 instructions :: [[Int]] -> CrochetCode
 instructions nbs = norm $ go [1] [1]
-    where
-        go :: [Int] -> [Int] -> CrochetCode
-        go tot []   = NoOp
-        go tot bdry = Then (nextRow current_stitches goal_stitches) (go (tot `union` next_row) next_row)
-            where
-                current_stitches :: Int
-                current_stitches = length bdry
+  where
+    go :: [Int] -> [Int] -> CrochetCode
+    go tot [] = NoOp
+    go tot bdry = Then (nextRow current_stitches goal_stitches) (go (tot `union` next_row) next_row)
+      where
+        current_stitches :: Int
+        current_stitches = length bdry
 
-                next_row :: [Int]
-                next_row = outerBoundary bdry tot nbs
+        next_row :: [Int]
+        next_row = outerBoundary bdry tot nbs
 
-                goal_stitches :: Int
-                goal_stitches = length next_row
-
-
+        goal_stitches :: Int
+        goal_stitches = length next_row
 
 -- TODO: Fix types?
 triple :: [a] -> (a, a, a)
